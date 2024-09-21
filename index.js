bfb8f5aacd396672ee7a3186f57e959f37460dcf
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-09-21 20:46:30 +0530

    refactor: connected routers

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const { UserRouter } = require("./routers/User.Router");
+const { CourseRouter } = require("./routers/Course.Router");
 
 const app = express();
 const PORT = 3000;
+
+app.use("/user", UserRouter);
+app.use("/course", CourseRouter);
 
 app.listen(PORT, () => {
   console.log("Course selling course");
