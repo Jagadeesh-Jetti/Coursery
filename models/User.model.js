9fff8e567f780be2d8c5a3c95452afaf823d3487
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-09-21 22:29:23 +0530

    feat: added user model

---
+++ models/User.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+const userSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      default: uuidv4,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
