aa1fea33c200448270e1259cd4c3e46fb4737f56
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-09-21 20:48:59 +0530

    refactor: updated routers

--- routers/User.Router.js
+++ routers/User.Router.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const userRouter = express.Router();
 
-app.post("/signup", function (req, res) {
+userRouter.post("/signup", function (req, res) {
   res.json({
     message: "sign up route",
   });
 });
 
-app.post("?signin", function (req, res) {
+userRouter.post("/signin", function (req, res) {
   res.json({
     message: "sign in route",
   });
