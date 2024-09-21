ad530b6cd0dabfef4f018ef3ed261bf1deb586da
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-09-21 20:47:41 +0530

    refactor: updated routes

--- routers/Course.Router.js
+++ routers/Course.Router.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const CourseRouter = express.Router();
 
-app.get("/courses", function (req, res) {
+CourseRouter.get("/courses", function (req, res) {
   res.json({
     message: "courses",
   });
 });
 
-app.post("/purchase", function (req, res) {
+CourseRouter.post("/purchase", function (req, res) {
   res.json({
     message: "purchase route",
   });
