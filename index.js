a3f50ad5afc2823d44a8502ecaf3d70eff525d2f
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-10-01 22:08:29 +0530

    refactor: updated the index file

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const { UserRouter } = require("./routers/User.Router");
-const { CourseRouter } = require("./routers/Course.Router");
+
+const UserRouter = require("./routers/User.Router");
+const CourseRouter = require("./routers/Course.Router");
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.use("/user", UserRouter);
 app.use("/course", CourseRouter);
 
-app.listen(PORT, () => {
-  console.log("Course selling course");
-});
-
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log("Course selling app");
 });
